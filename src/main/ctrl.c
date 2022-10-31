0085d3ec4aa75f49217663a0c69055e125fbcf65
Mickael Sergent <mickael.sergent@free.fr>
2022-10-31 21:04:40 +0100

    add first log of data in sd card

--- src/main/ctrl.c
+++ src/main/ctrl.c
@@ -3,6 +3,8 @@
 #include "filelog.h"
 #include "log.h"
 #include "data_defs.h"
+#include <time.h>
+#include <sys/time.h>
 
 #define CTRL_NB_MSG       (20)
 
@@ -10,6 +12,8 @@ static TaskHandle_t ctrl_taskHandle = NULL;
 static QueueHandle_t ctrl_dataReception;
 
 static void ctrl_task(void* arg);
+static void ctrl_log_data(data_msg_t* pDataMsg);
+static void ctrl_display_data_reception(data_msg_t* pDataMsg);
 
 STATUS ctrl_init(void)
 {
@@ -33,6 +37,77 @@ STATUS ctrl_init(void)
 QueueHandle_t ctrl_get_data_queue()
 {
   return ctrl_dataReception;
+}
+
+static void ctrl_task(void* arg)
+{
+  UNUSED(arg);
+  data_msg_t dataMsg;
+  BaseType_t rc;
+
+  while (1)
+  {
+    rc = xQueueReceive(ctrl_dataReception, &dataMsg, OS_SEC_TO_TICK(10));
+    if (rc == pdTRUE)
+    {
+      log_dbg_print("Data Reception");
+      ctrl_display_data_reception(&dataMsg);
+      ctrl_log_data(&dataMsg);
+    }
+    else
+    {
+      log_dbg_print("Timeout");
+    }
+  }
+}
+
+void ctrl_get_time(struct tm* pDate)
+{
+  struct timeval currentTime;
+  gettimeofday(&currentTime, NULL);
+  localtime_r(&currentTime.tv_sec, pDate);
+}
+
+static void ctrl_log_data(data_msg_t* pDataMsg)
+{
+  STATUS s;
+  struct tm dateTime;
+  filelog_msg* pMsg;
+  pMsg = filelog_allocate_msg();
+  if (pMsg != NULL)
+  {
+    ctrl_get_time(&dateTime);
+    switch (pDataMsg->type)
+    {
+      case RAIN:
+        snprintf(pMsg->data, FILELOG_STR_SIZE_MAX - 1, "d;rain;%.2d:%.2d:%.2d;%f\n", dateTime.tm_hour, dateTime.tm_min,
+                 dateTime.tm_sec, pDataMsg->value.f);
+        break;
+
+      case WIND_DIR:
+        snprintf(pMsg->data, FILELOG_STR_SIZE_MAX - 1, "d;wind_dir;%.2d:%.2d:%.2d;%d\n", dateTime.tm_hour, dateTime.tm_min,
+                 dateTime.tm_sec, pDataMsg->value.i);
+        break;
+
+      case WIND_SPEED:
+        snprintf(pMsg->data, FILELOG_STR_SIZE_MAX - 1, "d;wind_speed;%.2d:%.2d:%.2d;%f\n", dateTime.tm_hour, dateTime.tm_min,
+                 dateTime.tm_sec, pDataMsg->value.f);
+        break;
+
+      default:
+        pMsg->data[0] = '\0';
+        break;
+    }
+
+    s = filelog_write(pMsg);
+    if (s != STATUS_OK)
+      log_info_print("error trying to send msg\n");
+  }
+  else
+  {
+    log_info_print("error trying to allocate msg\n");
+  }
+
 }
 
 static void ctrl_display_data_reception(data_msg_t* pDataMsg)
@@ -60,38 +135,3 @@ static void ctrl_display_data_reception(data_msg_t* pDataMsg)
   }
 }
 
-static void ctrl_task(void* arg)
-{
-  UNUSED(arg);
-  STATUS s;
-  data_msg_t dataMsg;
-  BaseType_t rc;
-
-  while (1)
-  {
-    rc = xQueueReceive(ctrl_dataReception, &dataMsg, OS_SEC_TO_TICK(10));
-    if (rc == pdTRUE)
-    {
-      log_dbg_print("Data Reception");
-      ctrl_display_data_reception(&dataMsg);
-    }
-    else
-    {
-      log_dbg_print("Timeout");
-    }
-
-    filelog_msg* pMsg;
-    pMsg = filelog_allocate_msg();
-    if (pMsg != NULL)
-    {
-      snprintf(pMsg->data, FILELOG_STR_SIZE_MAX - 1, "test %d\n", tick_get());
-      s = filelog_write(pMsg);
-      if (s != STATUS_OK)
-        log_info_print("error trying to send msg\n");
-    }
-    else
-    {
-      log_info_print("error trying to allocate msg\n");
-    }
-  }
-}
